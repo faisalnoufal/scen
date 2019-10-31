34d6e53ead2582626bb030641975fa9d994345a1
Faisal Athikkai <faisalnoufal@gmail.com>
2019-10-31 08:51:53 +0530

    contact form updated

---
+++ contactform/send.php
@@ -0,0 +1,18 @@
+<?php
+	$userName 		= $_POST['Full_Name'];
+	$phoneNumber    = $_POST['Telephone_Number'];
+	$userEmail	 	= $_POST['Email_Address'];
+	$userMessage 		= $_POST['Your_Message'];
+	$to 			= "farizafaisal2007@gmail.com";
+	$subject 		= "Email from Scen Cyber Security Lab | Contact form";
+	$body 			= "Information Submitted:";
+	$body .= "\r\n Name: " . $userName;
+	$body .= "\r\n Phone: " . $phoneNumber;
+ 	$body .= "\r\n Email: " . $userEmail;
+	$body .= "\r\n Message: " . $userMessage;
+    mail($to, $subject, $body);
+?>
+
+<script type="text/javascript">
+    window.location = "/contactform/thankyou.html"
+</script>
